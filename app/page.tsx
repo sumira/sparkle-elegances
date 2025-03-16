88323b45bd3bd104950fcd10af9f5f6bef64db87
Sumira <sumiragp@gmail.com>
2025-03-16 17:20:01 +0530

    replace hero image with PNG format and adjust layout styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import React from "react";
-import HeroImage from "../assets/hero.jpg";
+import HeroImage from "../assets/hero.png";
 import Image from "next/image";
 import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <>
-      <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[650px] relative px-8">
+      <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[650px] relative ">
         {/*Brand Info*/}
         <motion.div
           initial={{ opacity: 0, x: 200 }}
@@ -15,7 +15,7 @@ export default function Home() {
           transition={{
             duration: 0.8,
           }}
-          className="flex flex-col justify-center py-14 px-4 md:pr-16 xl:pr-40 md:py-0"
+          className="flex flex-col justify-center py-14 px-24 md:pr-12  md:py-0"
         >
           <div className="text-center md:text-left space-y-4">
             <h1 className="text-pink-500 text-5xl lg:text-6xl font-bold !leading-tight ">
@@ -37,19 +37,21 @@ export default function Home() {
         </motion.div>
         {/*Product Image*/}
         <motion.div
-          className="flex justify-center items-center"
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
+          className="hidden md:flex justify-center items-center"
+          initial={{ opacity: 0, x: -200 }}
+          animate={{ opacity: 1, x: 0 }}
           transition={{
-            duration: 0.8,
+            type: "spring",
+            stiffness: 260,
             delay: 0.5,
-            ease: [0, 0.71, 0.2, 1.01],
+            duration: 0.8,
           }}
         >
           <Image
             src={HeroImage}
             alt="Hero Image"
-            className="w-[350px] md:w-[550px] xl:w-[700px]"
+            className="w-full h-auto object-contain"
+            style={{ maxHeight: "80vh", maxWidth: "100%" }}
           />
         </motion.div>
       </div>
